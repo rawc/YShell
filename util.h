b626e68ea028bd4c21da353ec6a8a0dd04a2acb3
rawc <louiechatta36@gmail.com>
2014-11-01 17:23:11 -0700

    Create util.h
    Holds a bunch of utility functions used all throughout the program

---
+++ util.h
@@ -0,0 +1,133 @@
+/*
+ *
+ * This assignment was completed doing paired programming by
+ * PARTNER: Louis Chatta (lchatta@ucsc.edu)
+ * PARTNER: Patrick Mathieu (pmathieu@ucsc.edu)
+ *
+ */
+
+
+//
+// util -
+//    A utility class to provide various services not conveniently
+//    included in other modules.
+//
+
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+using namespace std;
+
+//
+// Convenient type using to allow brevity of code elsewhere.
+//
+
+using wordvec = vector<string>;
+
+//
+// yshell_exn -
+//    Extend runtime_error for throwing exceptions related to this
+//    program.
+//
+
+class yshell_exn: public runtime_error {
+   public:
+      explicit yshell_exn (const string& what);
+      //explicit stops implicit conversions
+      //implicit means that wrong data type into method, and 
+      //conversion occurs
+};
+
+//
+// setexecname -
+//    Sets the static string to be used as an execname.
+// execname -
+//    Returns the basename of the executable image, which is used in
+//    printing error messags.
+//
+
+void execname (const string&);
+string& execname();
+
+
+//
+// want_echo -
+//    We want to echo all of cin to cout if either cin or cout
+//    is not a tty.  This helps make batch processing easier by
+//    making cout look like a terminal session trace.
+//
+
+bool want_echo();
+//controls the output of commands
+
+//
+// exit_status -
+//    A static class for maintaining the exit status.  The default
+//    status is EXIT_SUCCESS (0), but can be set to another value,
+//    such as EXIT_FAILURE (1) to indicate that error messages have
+//    been printed.
+//
+
+class exit_status {
+   private:
+      static int status;
+   public:
+      static void set (int);
+      static int get();
+};
+
+//
+// split -
+//    Split a string into a wordvec (as defined above).  Any sequence
+//    of chars in the delimiter string is used as a separator.  To
+//    Split a pathname, use "/".  To split a shell command, use " ".
+//
+
+wordvec split (const string& line, const string& delimiter);
+
+// complain -
+//    Used for starting error messages.  Sets the exit status to
+//    EXIT_FAILURE, writes the program name to cerr, and then
+//    returns the cerr ostream.  Example:
+//       complain() << filename << ": some problem" << endl;
+//
+
+ostream& complain();
+
+
+//
+// operator<< (vector) -
+//    An overloaded template operator which allows vectors to be
+//    printed out as a single operator, each element separated from
+//    the next with spaces.  The item_t must have an output operator
+//    defined for it.
+
+//what is an operator??
+template <typename item_t>
+//like generic in java^^^
+//vector<pair<int,int>> v = new v.....
+//if in java would be that^^
+//saves memory this way
+ostream& operator<< (ostream& out, const vector<item_t>& vec) {
+   string space = "";
+   for (const auto& item: vec) {
+      out << space << item;
+      space = " ";
+   }
+   return out;
+}
+//##
+//creates an operator "<<" which takes in a "stream" and
+//"generic" vector
+//then loops through entire vector
+//and saves into ostream
+//##
+//
+//this takes in a refrence of a vector, and then outputs
+//all indexes inside
+//needs to be refrence!! if not, will copy all
+#endif
